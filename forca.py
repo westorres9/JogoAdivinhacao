ac77b768cf28a0e0bfedaae0277d80514f61c82c
westorres9 <wester.torres9@gmail.com>
2021-12-08 10:50:05 -0200

    implementando jogo da forca

--- forca.py
+++ forca.py
@@ -5,6 +5,25 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_",]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    #enquanto true e true
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute =chute.strip()
+        i=0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                letras_acertadas[i] = letra
+                print(f'Encontrei a letra {chute} na posicao {i}')
+                print(letras_acertadas)
+            i = i + 1
 
 
 
