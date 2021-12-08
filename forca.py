cfb2fa49e8ffff97d6a061d318bc6639d03a0a14
westorres9 <wester.torres9@gmail.com>
2021-12-08 12:31:17 -0200

    implementada melhorias no jogo Forca

--- forca.py
+++ forca.py
@@ -1,12 +1,24 @@
+import random
 
 def jogar():
     print("*********************************")
-    print("***Bem vindo ao jogo da Forca***!")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "abacaxi".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
 
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -14,33 +26,30 @@ def jogar():
 
     print(letras_acertadas)
 
-    #enquanto true e true
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute =chute.strip().upper()
+        chute = chute.strip().upper()
 
         if(chute in palavra_secreta):
-            i=0
+            index = 0
             for letra in palavra_secreta:
                 if(chute == letra):
-                    letras_acertadas[i] = letra
-                    print(f'Encontrei a letra {chute} na posicao {i}')
-
-                i += 1
+                    letras_acertadas[index] = letra
+                index += 1
         else:
             erros += 1
 
-
-        enforcou = erros == len(palavra_secreta)
+        enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-    if (acertou):
-        print("Voce ganhou")
-    else:
-        print("Voce perdeu")
 
+    if(acertou):
+        print("Você ganhou!!")
+        print(f' A palavra secreta era {palavra_secreta}')
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
