63fd4e45ad75b579d36042ce0aa2b2e8ed6dd383
westorres9 <wester.torres9@gmail.com>
2021-12-08 00:03:44 -0200

    Jogo Adivinhacao implementado usando python

---
+++ forca.py
@@ -0,0 +1,14 @@
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca***!")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
